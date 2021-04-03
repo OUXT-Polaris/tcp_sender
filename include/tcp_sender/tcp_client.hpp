6ca911df39b0befb73f887b3ed27d7ac62222579
Masaya Kataoka <ms.kataoka@gmail.com>
2021-04-03 22:34:51 +0900

    initial commit
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

---
+++ include/tcp_sender/tcp_client.hpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2021 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TCP_SENDER__TCP_CLIENT_HPP_
+#define TCP_SENDER__TCP_CLIENT_HPP_
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+namespace tcp_sender
+{
+class TcpClient
+{
+public:
+  explicit TcpClient(
+    boost::asio::io_service & io_service);
+
+private:
+  void connect();
+  void onConnect(const boost::system::error_code & error);
+  boost::asio::io_service & io_service_;
+  boost::asio::ip::tcp::socket socket_;
+};
+}  // namespace tcp_sender
+
+#endif  // TCP_SENDER__TCP_CLIENT_HPP_
---
+++ src/tcp_client.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 2021 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <tcp_sender/tcp_client.hpp>
+
+namespace tcp_sender
+{
+TcpClient::TcpClient(
+  boost::asio::io_service & io_service)
+: io_service_(io_service), socket_(io_service_)
+{
+}
+
+void TcpClient::connect()
+{
+  socket_.async_connect(
+    boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 31400),
+    boost::bind(&TcpClient::onConnect, this, boost::asio::placeholders::error));
+}
+
+void onConnect(const boost::system::error_code & error)
+{
+  if (error) {
+    // std::cout << "connect failed : " << error.message() << std::endl;
+  } else {
+    // std::cout << "connected" << std::endl;
+  }
+}
+}  // namespace tcp_sender
